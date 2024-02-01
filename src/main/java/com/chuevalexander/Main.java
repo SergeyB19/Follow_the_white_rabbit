f0653bedb0832cb5349de1a8ba053b44088b7fd5
SergeyB19 <my.h.m@yandex.ru>
2024-02-01 14:29:40 +0300

    Введение в классы и объекты

--- src/main/java/com/chuevalexander/Main.java
+++ src/main/java/com/chuevalexander/Main.java
@@ -1,17 +1,25 @@
 package com.chuevalexander;
 
+import com.chuevalexander.lesson7.Bus;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Bus ourBus;
+        Bus firstBus;
+        Bus secondBus;
+        ourBus = new Bus();
+        firstBus = new Bus();
+        secondBus = new Bus();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        ourBus.color = "Green";
+        firstBus.color = "Red";
+        secondBus.color = "Grey";
+
+        ourBus.showColor();
+        firstBus.showColor();
+        secondBus.showColor();
+
     }
 }
