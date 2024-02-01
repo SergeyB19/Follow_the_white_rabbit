b226a53ee95c8e2079b19b7a7cf8b2e064d42f94
SergeyB19 <my.h.m@yandex.ru>
2024-02-01 19:55:01 +0300

    Ссылочные типы данных

---
+++ src/main/java/com/chuevalexander/lesson8/References.java
@@ -0,0 +1,11 @@
+package com.chuevalexander.lesson8;
+
+public class References {
+    String str = "Моя строка";
+    String str2 = new String("Моя строка");
+
+    public void foo() {
+        new String("Моя строка");
+    }
+
+}
