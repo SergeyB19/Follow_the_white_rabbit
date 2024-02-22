3a7c358c0283061991e05854f4ad1fe0dbe695eb
SergeyB19 <my.h.m@yandex.ru>
2024-02-22 10:02:29 +0300

    abstract

---
+++ src/main/java/com/chuevalexander/lesson22/AK47.java
@@ -0,0 +1,8 @@
+package com.chuevalexander.lesson22;
+
+public class AK47 extends Weapon {
+    @Override
+    public void shot() {
+
+    }
+}
---
+++ src/main/java/com/chuevalexander/lesson22/Weapon.java
@@ -0,0 +1,11 @@
+package com.chuevalexander.lesson22;
+
+public abstract class Weapon {
+    private String name;
+
+    public abstract void shot();
+
+    public void show() {
+        System.out.println("");
+    }
+}
